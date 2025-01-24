08c8ca75c969ff3e19091790d9f8856c2db78d17
pappferencgabor <p.feco2005@gmail.com>
2025-01-24 09:13:11 +0100

    push

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -13,5 +13,30 @@
 
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
 
+        public void AddProduct(string name, double price)
+        {
+            _products.Add(new Product(name, price));
+        }
+
+        public void RemoveProduct(string name) 
+        {
+            var product = _products.FirstOrDefault(p => p.Name == name);
+            if (product == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            _products.Remove(product);
+        }
+
+        public double GetTotalPrice()
+        {
+            return _products.Sum(p => p.Price);
+        }
+
+        public List<Product> GetProducts()
+        {
+            return _products;
+        }
     }
 }
